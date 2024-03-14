42db7b488c145fe539bf1ea66c2fcdb4bf9a5c73
Akarshmashal <mashalakarsh@gmail.com>
2024-03-14 18:27:55 +0530

    Added Program on constructor chaining

---
+++ Java/src/com/javac/Student.java
@@ -0,0 +1,27 @@
+package com.javac;
+
+public class Student 
+{
+	String name;
+	String pid;
+	double mockrating;
+	public Student(String name, String pid, double mockrating) 
+	{
+		this(name,pid);
+		this.pid = pid;
+	}
+	public Student(String name, String pid) 
+	{
+		this(name);
+		this.pid = pid;
+	}
+	public Student(String name) 
+	{
+		this.name = name;
+	}
+	public static void main(String[] args) 
+	{
+		Student s1=new Student("Akarsh");
+		
+	}
+}
